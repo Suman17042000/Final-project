1c631727bb77b8eb6d8718107c0775d1cfdab432
Suman Prasad <sumankumarprasad16@gmail.com>
2023-07-21 21:39:52 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Reactdom from "react-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import App from './Assignment/App';
+
+Reactdom.render(<App/>,document.getElementById("root"));
